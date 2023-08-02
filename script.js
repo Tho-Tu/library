dc199d16ac72e658fd506b23ff6a463c854544d2
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-08-02 14:11:16 +1000

    Add delete button function to each book card

--- script.js
+++ script.js
@@ -11,53 +11,95 @@ function addBookToLibrary(bookObject) {
   myLibrary.push(bookObject);
 }
 
-// const bookOne = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
+const bookOne = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
 
-// const bookTwo = new Book(
-//   "Never Split the Difference",
-//   "Chris Voss",
-//   "274",
-//   true
-// );
+const bookTwo = new Book(
+  "Never Split the Difference",
+  "Chris Voss",
+  "274",
+  true
+);
 
-// const bookThree = new Book("Zero to One", "Peter Thiel", "210", true);
+const bookThree = new Book("Zero to One", "Peter Thiel", "210", true);
 
-// addBookToLibrary(bookOne);
-// addBookToLibrary(bookTwo);
-// addBookToLibrary(bookThree);
-// displayBooks();
-
-const bookCardSection = document.querySelector("#book-card-section");
+addBookToLibrary(bookOne);
+addBookToLibrary(bookTwo);
+addBookToLibrary(bookThree);
 
 function displayBooks() {
+  const bookCardSection = document.querySelector("#book-card-section");
   bookCardSection.textContent = "";
+
+  let bookIndex = 0;
   for (const bookObject of myLibrary) {
     const newBook = document.createElement("div");
     newBook.classList.add("book");
 
+    // title
     const newTitle = document.createElement("div");
     newTitle.classList.add("display-title");
     newTitle.textContent = `${bookObject.title}`;
     newBook.appendChild(newTitle);
 
+    // author
     const newAuthor = document.createElement("div");
     newAuthor.classList.add("display-author");
     newAuthor.textContent = `${bookObject.author}`;
     newBook.appendChild(newAuthor);
 
+    // pages
     const newPages = document.createElement("div");
     newPages.classList.add("display-pages");
     newPages.textContent = `${bookObject.pages} Pages`;
     newBook.appendChild(newPages);
 
+    // read status
     const newRead = document.createElement("div");
+    const newReadLabel = document.createElement("label");
+    const newReadCheckBox = document.createElement("input");
+
     newRead.classList.add("display-read");
-    newRead.textContent = `Read: ${bookObject.read}`;
+    newReadLabel.setAttribute(
+      "for",
+      `${bookObject.title.replace(/\s+/g, "-").toLowerCase()}-read`
+    );
+    newReadLabel.textContent = `Read: `;
+
+    newReadCheckBox.setAttribute("type", "checkbox");
+    newReadCheckBox.setAttribute(
+      "id",
+      `${bookObject.title.replace(/\s+/g, "-").toLowerCase()}-read`
+    );
+    newReadCheckBox.checked = bookObject.read;
+
+    newRead.appendChild(newReadLabel);
+    newRead.appendChild(newReadCheckBox);
     newBook.appendChild(newRead);
+
+    // delete button
+    const newDelete = document.createElement("button");
+    newDelete.classList.add("display-delete");
+    newDelete.setAttribute("type", "button");
+    newDelete.textContent = "Delete";
+    newDelete.setAttribute("data-index", `${bookIndex}`);
+    bookIndex += 1;
+    newBook.appendChild(newDelete);
 
     bookCardSection.appendChild(newBook);
   }
+  // delete book
+  const bookDeleteButtons = document.querySelectorAll("[data-index]");
+
+  bookDeleteButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", () => {
+      let index = deleteButton.getAttribute("data-index");
+      myLibrary.splice(index, 1);
+      displayBooks();
+    });
+  });
 }
+
+displayBooks();
 
 // displays/hides the form input
 const displayForm = document.querySelector("#add-new-book");
@@ -100,3 +142,5 @@ userForm.addEventListener(
   },
   false
 );
+
+// toggle book's read status on object
